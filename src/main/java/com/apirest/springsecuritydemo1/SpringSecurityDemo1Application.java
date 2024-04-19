82ec087ef1d2ab20f7337e6905f3667caeff72c9
Daniel Penelva <d4n.andrade@gmail.com>
2024-04-19 20:28:47 -0300

    cors - criando index-html para testar a requisição GET e AJAX para liberação do CORS + criando Configuração centralizada do CORS na classe SpringSecurityDemo1Application

--- src/main/java/com/apirest/springsecuritydemo1/SpringSecurityDemo1Application.java
+++ src/main/java/com/apirest/springsecuritydemo1/SpringSecurityDemo1Application.java
@@ -8,7 +8,9 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EntityScan(basePackages = {"com.apirest.springsecuritydemo1.entities"})
@@ -18,10 +20,23 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @EnableWebMvc
 @RestController
 @EnableAutoConfiguration
-public class SpringSecurityDemo1Application {
+public class SpringSecurityDemo1Application implements WebMvcConfigurer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringSecurityDemo1Application.class, args);
+	}
+
+	/* Cross Origin - Configuração centralizada */
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+
+		// libera acesso a todos os endpoints do usuario do RestController "IndexController" e todos os métodos (POST, GET, PUT, DELETE). E liberar acesso a todos os servidores que poderá fazer a requisição.
+		registry.addMapping("/usuario/**")
+                        .allowedOrigins("*") // Permit all origins
+                        .allowedMethods("GET", "POST", "PUT", "DELETE") // Allow common HTTP methods
+                        .allowedHeaders("*"); // Permit all headers
+
+		// OBS. Caso queira liberar para um unico servidor para que possa fazer a requisição basta colocar "http://localhost:8080" - exemplo: .allowedOrigins("http://localhost:8080") 
 	}
 
 }
