5636db1765c06baa52cf1204a3302c9dd9bb6af0
Daniel Penelva <d4n.andrade@gmail.com>
2024-04-19 19:36:29 -0300

    Configurando o contexto da aplicação (application.properties) + Criando os métodos GET, PUT, POST e DELETE na classe IndexController

--- src/main/java/com/apirest/springsecuritydemo1/controllers/IndexController.java
+++ src/main/java/com/apirest/springsecuritydemo1/controllers/IndexController.java
@@ -1,50 +1,76 @@
 package com.apirest.springsecuritydemo1.controllers;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.apirest.springsecuritydemo1.entities.Usuario;
+import com.apirest.springsecuritydemo1.repositories.UsuarioRepository;
+
+import lombok.AllArgsConstructor;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/usuario")
+@AllArgsConstructor
 public class IndexController {
 
-    // http://localhost:8080/api/
-    @GetMapping(value = "/", produces = "application/json")
-    public ResponseEntity init() {
-        Usuario usuario = Usuario.builder()
-                .id(1L)
-                .login("daniel")
-                .senha("123")
-                .nome("Daniel")
-                .build();
+    private UsuarioRepository usuarioRepository;
 
-        Usuario usuario2 = Usuario.builder()
-                .id(2L)
-                .login("biana")
-                .senha("123")
-                .nome("Biana")
-                .build();
-
-        List<Usuario> usuarios = new ArrayList<>();
-        usuarios.add(usuario);
-        usuarios.add(usuario2);
-
-        return ResponseEntity.ok(usuarios);
-
-        // ou pode fazer assim - exemplo2:
-        // return new ResponseEntity(usuarios, HttpStatus.OK);
-
+    // http://localhost:8080/projeto-api-rest/usuario/{id}
+    @GetMapping(value = "/{id}", produces = "application/json")
+    public ResponseEntity getByIdUsuario(@PathVariable("id") Long id) {
+        return new ResponseEntity(usuarioRepository.findById(id), HttpStatus.OK);
     }
 
-    // http://localhost:8080/api/teste?nome=Daniel&sobrenome=Penelva
+    // http://localhost:8080/projeto-api-rest/usuario/all
+    @GetMapping(value = "/all", produces = "application/json")
+    public ResponseEntity<List<Usuario>> getAllUsuarios() {
+        return new ResponseEntity<List<Usuario>>(usuarioRepository.findAll(), HttpStatus.OK);
+    }
+
+    // http://localhost:8080/projeto-api-rest/usuario/create
+    @PostMapping(value = "/create", produces = "application/json")
+    public ResponseEntity<Usuario> createUsuario(@RequestBody Usuario usuario) {
+        return new ResponseEntity<Usuario>(usuarioRepository.save(usuario), HttpStatus.CREATED);
+    }
+
+    // http://localhost:8080/projeto-api-rest/usuario/replace/{id}
+    @PutMapping(value = "/replace/{id}", produces = "application/json")
+    public ResponseEntity<Usuario> updateUsuario(@PathVariable("id") Long id, @RequestBody Usuario updateUsuario) {
+        Usuario usuarioId = usuarioRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Id: " + id + " não encontrado"));
+
+        Usuario newUsuario = new Usuario();
+        newUsuario.setId(usuarioId.getId());
+        newUsuario.setLogin(updateUsuario.getLogin());
+        newUsuario.setSenha(updateUsuario.getSenha());
+        newUsuario.setNome(updateUsuario.getNome());
+
+        return new ResponseEntity<Usuario>(usuarioRepository.save(newUsuario), HttpStatus.OK);
+    }
+
+    // http://localhost:8080/projeto-api-rest/usuario/delete/{id}
+    @DeleteMapping(value = "/delete/{id}", produces = "application/json")
+    public ResponseEntity deleteUsuario(@PathVariable("id") Long id) {
+
+        Usuario usuarioId = usuarioRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Id: " + id + " não encontrado"));
+
+        usuarioRepository.delete(usuarioId);
+        return ResponseEntity.noContent().build();
+    }
+
+    // http://localhost:8080/projeto-api-rest/usuario/teste?nome=Daniel&sobrenome=Penelva
     @GetMapping(value = "/teste", produces = "application/json")
     public ResponseEntity seuNomeCompleto(
             @RequestParam(value = "nome", defaultValue = "nome não declarado") String nome,
@@ -52,5 +78,4 @@ public class IndexController {
         System.out.println("Nome: " + nome + " - Sobrenome: " + sobrenome);
         return new ResponseEntity("Nome: " + nome + " - Sobrenome: " + sobrenome, HttpStatus.OK);
     }
-
 }
