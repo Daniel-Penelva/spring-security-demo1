d6f48eea2ac5f11182b2a4035a584645346fc365
Daniel Penelva <d4n.andrade@gmail.com>
2024-04-19 18:56:04 -0300

    manipulando json na tela através do objeto Usuario + alterando entidade Usuario

--- src/main/java/com/apirest/springsecuritydemo1/controllers/IndexController.java
+++ src/main/java/com/apirest/springsecuritydemo1/controllers/IndexController.java
@@ -1,11 +1,16 @@
 package com.apirest.springsecuritydemo1.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.apirest.springsecuritydemo1.entities.Usuario;
 
 @RestController
 @RequestMapping("/api")
@@ -14,7 +19,29 @@ public class IndexController {
     // http://localhost:8080/api/
     @GetMapping(value = "/", produces = "application/json")
     public ResponseEntity init() {
-        return new ResponseEntity("Olá Mundo!", HttpStatus.OK);
+        Usuario usuario = Usuario.builder()
+                .id(1L)
+                .login("daniel")
+                .senha("123")
+                .nome("Daniel")
+                .build();
+
+        Usuario usuario2 = Usuario.builder()
+                .id(2L)
+                .login("biana")
+                .senha("123")
+                .nome("Biana")
+                .build();
+
+        List<Usuario> usuarios = new ArrayList<>();
+        usuarios.add(usuario);
+        usuarios.add(usuario2);
+
+        return ResponseEntity.ok(usuarios);
+
+        // ou pode fazer assim - exemplo2:
+        // return new ResponseEntity(usuarios, HttpStatus.OK);
+
     }
 
     // http://localhost:8080/api/teste?nome=Daniel&sobrenome=Penelva
--- src/main/java/com/apirest/springsecuritydemo1/entities/Usuario.java
+++ src/main/java/com/apirest/springsecuritydemo1/entities/Usuario.java
@@ -5,10 +5,12 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @Entity
 @Getter
@@ -16,6 +18,8 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 @EqualsAndHashCode(of = "id")
+@Builder
+@ToString
 public class Usuario {
 
     @Id
